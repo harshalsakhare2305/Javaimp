6141d19346a572bdae63787d61ba3b40c7c79912
harshal Sakhare <118835450+harshalsakhare2305@users.noreply.github.com>
2023-10-31 18:58:46 +0530

    Create Trapping_RainWater.java

---
+++ Trapping_RainWater.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int trap(int[] height) {
+        int n= height.length;
+        int left =0;
+        int right =n-1;
+        int leftmax =0;
+        int rightmax =0;
+        int res=0;
+
+        while(left <=right){
+        if(height[left]<=height[right]){
+            if(height[left]>=leftmax){
+            leftmax =height[left];
+        }else{
+           res+=(leftmax-height[left]);
+        }
+
+        left++;
+        }else{
+            if(height[right]>=rightmax){
+                 rightmax =height[right];
+            }else{
+                 res+=(rightmax-height[right]);
+            }
+            right--;
+        }
+        }
+
+        return res;
+    }
+}
